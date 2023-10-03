6042d7590263bea3a9faaacdafc77553cd57ffd9
chase <chasegoodrow@gmail.com>
2023-10-03 13:44:50 -0600

    added local storage for tasks and their strikethrough status

--- index.js
+++ index.js
@@ -1,42 +1,73 @@
+const itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : []
+const itemsStikedThrough = localStorage.getItem('strikes') ? JSON.parse(localStorage.getItem('strikes')) : []
+console.log(itemsArray);
+console.log(itemsStikedThrough);
 
+document.querySelector('#push').addEventListener("click", () =>{
+    const item = document.querySelector('#item');
+    createItem(item);
+});
 
+function createItem(item){
+    itemsArray.push(item.value);
+    localStorage.setItem("items", JSON.stringify(itemsArray));
+    itemsStikedThrough.push(false);
+    localStorage.setItem("strikes", JSON.stringify(itemsStikedThrough));
+    location.reload();
+}
 
-
-document.querySelector('#push').onclick = function(){
-    if(document.querySelector('#newtask input').value.length == 0){
-        alert("Kindly Enter Task Name!!!!")
-    }
-
-    else{
-        document.querySelector('#tasks').innerHTML += `
+function displayTasks(item){  
+    let tasks = "";
+    for (let i=0; i < itemsArray.length; i++){
+        tasks += `
             <div class="task">
-                <button class='crossOutButton'>✔️
-                    <i class="crossOutButton"></i>
-                </button>
-                <span id="taskname">
-                    ${document.querySelector('#newtask input').value}
-                </span>
-                <button class="delete">🗑️
-                    <i class="far fa-trash-alt"></i>
-                </button>
+                <button class='crossOutButton'>✔️</button>
+                <span id="taskname">${itemsArray[i]}</span>
+                <button class="delete">🗑️</button>
             </div>
         `;
-
-        var current_tasks = document.querySelectorAll(".delete");
-        for(var i=0; i<current_tasks.length; i++){
-            current_tasks[i].onclick = function(){
-                this.parentNode.remove();
-            }
-        }
-        var crossout_tasks = document.querySelectorAll(".crossOutButton");
-        for(var i=0; i<crossout_tasks.length; i++){
-            crossout_tasks[i].onclick = function(){
-                if (this.parentNode.children[1].style.textDecoration === ('line-through')) {
-                    this.parentNode.children[1].style.textDecoration = ('none')
-                } else {
-                    this.parentNode.children[1].style.textDecoration = ('line-through');
-                }
-            }
-        }
     }
+    document.querySelector('#tasks').innerHTML = tasks
+    activateDeleteButtons()
+    activateCrossOutButtons()
+}
+
+function activateDeleteButtons(){
+    let delButton = document.querySelectorAll(".delete")
+    delButton.forEach((button, i) => {
+        button.addEventListener("click", () => { deleteItem(i) })
+    })
+}
+
+function deleteItem(i){
+    itemsArray.splice(i, 1)
+    itemsStikedThrough.splice(i, 1)
+    localStorage.setItem("items", JSON.stringify(itemsArray))
+    localStorage.setItem("strikes", JSON.stringify(itemsStikedThrough))
+    location.reload()
+}
+
+function activateCrossOutButtons(){
+    let CrossOutButton = document.querySelectorAll(".crossOutButton")
+    CrossOutButton.forEach((button, i) => {
+        button.addEventListener("click", () => { saveItemCrossed(i) })
+    })
+}
+function saveItemCrossed(i){
+    if (!itemsStikedThrough[i]) itemsStikedThrough[i] = true
+    else itemsStikedThrough[i] = false
+    localStorage.setItem("strikes", JSON.stringify(itemsStikedThrough))
+    location.reload()
+}
+function crossOutItems(){
+    for (let i=0; i < itemsStikedThrough.length; i++){
+        if (itemsStikedThrough[i]){
+        document.querySelectorAll(".crossOutButton")[i].parentNode.children[1].style.textDecoration = ('line-through')
+        }       
+    }
+}
+
+window.onload = function(){
+    displayTasks()
+    crossOutItems()
 }
